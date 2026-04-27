15331ba480005ca45298facd3c6257edcfbe5c40
Serhii <146414705+SerhiiKysil@users.noreply.github.com>
2026-04-28 00:02:12 +0300

    again fix

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -9,6 +9,16 @@ output "web_dashboard_url" {
   description = "URL для доступу до веб-інтерфейсу"
 }
 
+output "grafana_url" {
+  value       = "http://${data.azurerm_public_ip.public_ip.ip_address}:3000"
+  description = "URL для доступу до Grafana"
+}
+
+output "prometheus_url" {
+  value       = "http://${data.azurerm_public_ip.public_ip.ip_address}:9090"
+  description = "URL для доступу до Prometheus"
+}
+
 output "ssh_command" {
   value       = "ssh -i private_key.pem azureuser@${data.azurerm_public_ip.public_ip.ip_address}"
   description = "Команда для підключення до ВМ по SSH"
